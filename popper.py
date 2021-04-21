7794f525546d0c4ceaa5b1ecd66ca4930d98b363
Andrew Cropper <andrew.cropper@gmail.com>
2021-04-21 09:30:48 +0100

    tidied up debugging and stats

--- popper.py
+++ popper.py
@@ -8,13 +8,13 @@ from popper.core import Clause, Literal
 
 def ground_constraints(grounder, max_clauses, max_vars, constraints):
     for constraint in constraints:
-        # find all bindings for the variables in the constraint
+        # find bindings for variables in the constraint
         assignments = grounder.ground_program(constraint, max_clauses, max_vars)
 
-        # build the clause object
+        # build the clause
         clause = Clause(constraint.head, tuple(lit for lit in constraint.body if isinstance(lit, Literal)))
 
-        # for each variable assignment, ground the clause
+        # ground the clause for each variable assignment
         for assignment in assignments:
             yield clause.ground(assignment)
 
@@ -25,42 +25,38 @@ def pprint(program):
 
 # @profile
 def popper(experiment):
-    with experiment.duration('basic setup'):
-        solver = Clingo(experiment)
-        tester = Tester(experiment)
-        grounder = solver
-        constrain = Constrain(experiment)
+    solver = Clingo(experiment)
+    tester = Tester(experiment)
+    grounder = solver
+    constrainer = Constrain(experiment)
 
+    iteration = 1
     for size in range(1, experiment.args.max_literals + 1):
         if experiment.args.debug:
-            print(f'MAX LITERALS: {size}')
+            print(f'{"*" * 20} MAX LITERALS: {size} {"*" * 20}')
 
         solver.update_number_of_literals(size)
-        iteration = 1
 
         while True:
-            if experiment.args.debug:
-                print(f'Iteration: {iteration}')
             experiment.total_programs += 1
 
             # 1. Generate
             with experiment.duration('generate'):
                 program = generate_program(solver)
             if program == None:
-                if experiment.args.debug:
-                    print(f'EXITS iteration {"*" * 50}\n\n')
                 break
+            # AC: TODO ORDER BY DEFAULT
             program.to_ordered()
 
             # 2. Test
             with experiment.duration('test'):
                 program_outcomes = tester.test(program)
             if experiment.args.debug:
-                print('Outcomes ->')
-                print('  Program:')
+                print(f'Program {iteration}:')
                 for clause in program.to_code():
-                    print('  ', clause)
-                print('  Outcome: ', program_outcomes[program])
+                    print(clause)
+                # AC: TODO - replace with confusion matrix
+                print('Outcome: ', program_outcomes[program])
             if program_outcomes[program] == (Outcome.ALL, Outcome.NONE):
                 if experiment.args.debug:
                     print()
@@ -70,21 +66,20 @@ def popper(experiment):
                 return
 
             # 3. Build constraints
-            with experiment.duration('build constraints'):
-                constraints = list(constrain.build_constraints(program_outcomes))
+            constraints = list(constrainer.build_constraints(program_outcomes))
 
             if experiment.args.debug:
-                print('Constraints ->')
+                print('Constraints:')
                 for index, constraint in enumerate(constraints, 1):
-                    print(f'  {index}. {constraint}')
+                    print(f'{index}. {constraint}')
                 print()
 
             # 4. Ground constraints
-            with experiment.duration('ground constraints'):
+            with experiment.duration('grounding'):
                 constraints = list(ground_constraints(grounder, solver.max_clauses, solver.max_vars, constraints))
 
             # 5. Add to the solver
-            with experiment.duration('ground clauses'):
+            with experiment.duration('adding'):
                 solver.add_ground_clauses(constraints)
 
             iteration += 1
--- popper/log.py
+++ popper/log.py
@@ -5,15 +5,12 @@ from contextlib import contextmanager
 def parse_args():
     parser = argparse.ArgumentParser(description='Popper, an ILP engine based on learning from failures')
     parser.add_argument('kbpath', help = 'Path to the knowledge base one wants to learn on')
-
     parser.add_argument('--eval-timeout', type=float, default=0.1, help='Prolog evaluation timeout in seconds')
     parser.add_argument('--max-literals', type=int, default=100, help='Maximum number of literals allowed in program')
     parser.add_argument('--minimal-testing', type=bool, default=True, help='Use minimal testing during testing')
     parser.add_argument('--no-pruning', type=bool, default=False, help='Use pruning during constrain stage')
-
-    parser.add_argument('--debug', default=False, help='Print debugging information to stderr')
-    parser.add_argument('--stats', default= False, help='Print statistics at end of execution')
-    
+    parser.add_argument('--debug', default=False, action='store_true', help='Print debugging information to stderr')
+    parser.add_argument('--stats', default= False, action='store_true', help='Print statistics at end of execution')
     return parser.parse_args()
 
 class Experiment:
@@ -21,9 +18,8 @@ class Experiment:
         self.args = parse_args()
         self.total_programs = 0
         self.durations = {}
-
         self.exec_start = perf_counter()
-    
+
     def __enter__(self):
         return self
 
@@ -40,7 +36,7 @@ class Experiment:
                 self.durations[operation] = [duration]
             else:
                 self.durations[operation].append(duration)
-            
+
     def stats(self, program):
         total_exec_time = perf_counter() - self.exec_start
 
@@ -56,5 +52,5 @@ class Experiment:
             if operation != 'basic setup': total_op_time += total
         message += f'Total operation time: {total_op_time:0.2f}s\n'
         message += f'Total execution time: {total_exec_time:0.2f}s\n'
-        
+
         print(message)
