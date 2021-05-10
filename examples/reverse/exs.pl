3a993d3bd937f22045df76bcbe36494319704318
Andrew Cropper <andrew.cropper@gmail.com>
2021-05-10 06:45:07 +0100

    Update exs.pl

--- examples/reverse/exs.pl
+++ examples/reverse/exs.pl
@@ -7,14 +7,3 @@ neg(f([a,b,c],[])).
 neg(f([a,b],[a])).
 neg(f([a,b,c],[b,c])).
 neg(f([1,2,3,4],[1,4,3,2])).
-
-
-
-f(A,B):-empty(A),empty(B).
-f(A,B):-head(A,C),tail(A,D),f(D,E),my_append(E,C,B).
-
-%% :-[bk].
-%% :-
-%%     forall(pos(X),(call(X))).
-
-%% %% f([],[]).
