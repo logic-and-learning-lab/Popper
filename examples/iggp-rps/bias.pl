36d9e36a4c3c29e5d0826e5dfe5d9466aa26ef99
Andrew Cropper <andrew.cropper@gmail.com>
2021-09-13 14:48:49 +0100

    small change of examples

--- examples/iggp-rps/bias.pl
+++ examples/iggp-rps/bias.pl
@@ -1,34 +1,36 @@
-%% SOLUTION:
-%% next_score(A,B,C):-different(B,D),my_true_score(A,B,C),wins(A,D)
-%% next_score(A,B,C):-my_true_score(A,B,D),wins(A,B),my_succ(D,C)
-%% next_score(A,B,C):-does(A,B,D),different(E,B),my_true_score(A,B,C),does(A,E,D)
-%% Total programs: 8444
+%% BEST PROGRAM:
+%% next_score(A,B,C):-my_true_score(A,B,D),my_succ(D,C),wins(A,B)
+%% next_score(A,B,C):-wins(A,D),my_true_score(A,B,C),different(B,D)
+%% next_score(A,B,C):-does(A,D,E),my_true_score(A,B,C),different(B,D),does(A,B,E)
+%% TP: 108, FN: 0, TN: 356, FP: 0
+
+%% Total programs: 1607
 %% Generate:
-%%     Called: 8456 times   Total: 13.57    Mean: 0.002     Max: 0.246
+%%     Called: 1619 times   Total: 4.64     Mean: 0.003     Max: 0.041
 %% Test:
-%%     Called: 8444 times   Total: 8.76     Mean: 0.001     Max: 0.032
+%%     Called: 1607 times   Total: 7.98     Mean: 0.005     Max: 0.048
 %% Build_Rules:
-%%     Called: 8443 times   Total: 1.37     Mean: 0.000     Max: 0.006
+%%     Called: 1606 times   Total: 3.20     Mean: 0.002     Max: 0.009
 %% Ground:
-%%     Called: 8443 times   Total: 0.67     Mean: 0.000     Max: 0.003
+%%     Called: 1606 times   Total: 0.26     Mean: 0.000     Max: 0.003
 %% Add:
-%%     Called: 8443 times   Total: 9.99     Mean: 0.001     Max: 0.005
-%% Total operation time: 34.38s
-%% Total execution time: 34.70s
+%%     Called: 1606 times   Total: 1.83     Mean: 0.001     Max: 0.004
+%% Test_Individual_Rules.Is_Totally_Incomplete:
+%%     Called: 1539 times   Total: 0.05     Mean: 0.000     Max: 0.001
+%% Total operation time: 17.96s
+%% Total execution time: 18.08s
 
 max_clauses(4).
 max_vars(5).
 max_body(7).
 
 head_pred(next_score,3).
-%% body_pred(draws,2).
 body_pred(wins,2).
-%% body_pred(loses,2).
 body_pred(my_true_score,3).
 body_pred(my_succ,2).
 body_pred(does,3).
 body_pred(beats,2).
-%% body_pred(player,1).
+body_pred(player,1).
 body_pred(different,2).
 
 type(next_score,(ex,player,int)).
@@ -41,11 +43,6 @@ type(does,(ex,player,action)).
 type(beats,(action,action)).
 type(player,(player,)).
 type(different,(player,player)).
-
-functional(beats,2).
-functional(my_succ,2).
-irreflexive(my_succ,2).
-irreflexive(beats,2).
 
 %% HACK BECAUSE WE DO NOT LEARN FROM INTERPRETATIONS
 :-
