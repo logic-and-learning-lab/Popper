b26ea1f349159be132d598f6e73034b12c0ae9df
polyphemus <rolfmorel@gmail.com>
2022-07-02 13:51:17 +0200

    Add context-dependent graph reachability example

---
+++ examples/context-dependent-monotonic-graph-reachability/exs.pl
@@ -0,0 +1,11 @@
+pos(path(g1,1,4)).
+pos(path(g1,5,7)).
+pos(path(g2,1,3)).
+pos(path(g2,2,5)).
+pos(path(g3,1,3)).
+neg(path(g1,2,1)).
+neg(path(g1,1,7)).
+neg(path(g2,3,1)).
+neg(path(g2,4,2)).
+neg(path(g2,4,3)).
+neg(path(g3,3,3)).
