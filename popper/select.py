9f2861cd4303fe8fdcfdf60341ece319d83e1162
Andrew Cropper <andrew.cropper@gmail.com>
2022-08-08 10:27:19 +0100

    vastly improved the combine stage by not delegating some reasoning to Clingo

--- popper/select.py
+++ popper/select.py
@@ -1,77 +1,105 @@
 import clingo
+import time
+from z3 import *
 from . util import format_rule, prog_size, format_prog, flatten, reduce_prog
 
-FIND_SUBSET_PROG = """
+# for when we do not yet have a complete solution
+FIND_SUBSET_PROG1 = """
 #show rule/1.
 #show incomplete/0.
 {rule(R)}:-size(R,_).
 covered(E):- covers(R,E), rule(R).
-different(R1,R2):- R1 != R2, size(R1,_), size(R2,_), covers(R1,E), not covers(R2,E).
-same(R1,R2):- R1 < R2, size(R1,_), size(R2,_), not different(R1,R2), not different(R2,R1).
-dominates(R1,R2):- different(R1,R2), not different(R2,R1).
 incomplete:- example(E), not covered(E).
-:- same(R1,R2), rule(R1), rule(R2).
-:- same(R1,R2), size(R1,K1), size(R2,K2), K1 >= K2, rule(R1).
-:- dominates(R1,R2), size(R1,K1), size(R2,K2), K1 <= K2, rule(R2).
 :~ example(E), not covered(E). [1@2, (E,)]
+:~ rule(R),size(R,K). [K@1, (R,)]
+"""
+
+# for when we have a complete solution
+# same as above but no weak constraint over examples covered
+FIND_SUBSET_PROG2 = """
+#show rule/1.
+{rule(R)}:-size(R,_).
+covered(E):- covers(R,E), rule(R).
+:- example(E), not covered(E).
 :~ rule(R),size(R,K). [K@1, (R,)]
 """
 
 class Selector:
     def __init__(self, settings, tester):
         self.settings = settings
+        self.tester = tester
+        self.build_example_encoding()
+
         self.prog_coverage = {}
+        self.prog_sizes = {}
 
         self.solution_found = False
         self.best_prog = None
-
         self.num_covered = 0
-
-        self.index_to_prog = {}
         self.max_size = None
 
-        self.prog_encoding = ''
-        self.example_to_hash = {}
-        self.prog_count = 0
-        self.build_example_encoding()
-        self.tester = tester
         self.constraints = []
 
+        self.success_sets = {}
+        self.shit_progs = set()
+
     def build_example_encoding(self):
+        self.example_to_hash = {}
         example_prog = []
         for i, x in enumerate(self.settings.pos):
             self.example_to_hash[x] = i
             example_prog.append(f'example({i}).')
         self.example_prog = '\n'.join(example_prog)
 
-    def build_prog_encoding(self, prog):
-        self.prog_count += 1
-        self.index_to_prog[self.prog_count] = prog
-        size = prog_size(prog)
-        prog_builder = []
-        prog_builder.append(f'size({self.prog_count},{size}).')
-        for ex in self.prog_coverage[prog]:
-            i = self.example_to_hash[ex]
-            prog_builder.append(f'covers({self.prog_count},{i}).')
-        self.prog_encoding += '\n'.join(prog_builder) + '\n'
+    # @profile
+    def update_shit_progs(self, prog, coverage, size):
+        if coverage in self.success_sets:
+            is_shit = False
+            for prog2, size2 in self.success_sets[coverage]:
+                if size2 <= size:
+                    self.shit_progs.add(prog)
+                    is_shit = True
+                    break
+                else:
+                    self.shit_progs.add(prog2)
+            if not is_shit:
+                self.success_sets[coverage].add((prog, size))
+        else:
+            self.success_sets[coverage] = set([(prog, size)])
 
+    # @profile
     def select_solution(self):
-        encoding = [FIND_SUBSET_PROG, '\n'.join(self.constraints), self.example_prog, self.prog_encoding]
+        encoding = []
 
         if self.solution_found:
-            # add size constraint
+            encoding.append(FIND_SUBSET_PROG2)
+            # add size constraint - although I am unsure whether this constraint helps in practice
             encoding.append(':- #sum{K,R : rule(R), size(R,K)} >= ' + f'{self.max_size}.')
-            # add hard constraint on example coverage
-            encoding.append(':- incomplete.')
         else:
-            encoding.append(':- #sum{1,E : covered(E)} <= ' + f'{self.num_covered}.')
+            encoding.append(FIND_SUBSET_PROG1)
+            # encoding.append(':- #sum{1,E : covered(E)} <= ' + f'{self.num_covered}.')
 
+        # add constraints to prune inconsistent recursive programs
+        encoding.extend(self.constraints)
+
+        self.index_to_prog = {}
+        prog_count = 0
+        for prog, examples_covered in self.prog_coverage.items():
+            if prog in self.shit_progs:
+                continue
+            k = prog_count
+            self.index_to_prog[k] = prog
+            size = self.prog_sizes[prog]
+            encoding.append(f'size({k},{size}).')
+            for ex in self.prog_coverage[prog]:
+                i = self.example_to_hash[ex]
+                encoding.append(f'covers({k},{i}).')
+            prog_count+=1
+
+        encoding.append(self.example_prog)
         encoding = '\n'.join(encoding)
 
-        # with open('sat-problem.pl', 'w') as f:
-            # f.write(encoding)
-        # print(encoding)
-        solver = clingo.Control()
+        solver = clingo.Control(["--opt-strategy=usc"])
         solver.add('base', [], encoding)
         solver.ground([('base', [])])
         out = []
@@ -91,17 +119,24 @@ class Selector:
                 # print('model', incomplete)
                 if incomplete:
                     continue
+                if not self.settings.recursion_enabled and not self.settings.pi_enabled:
+                    continue
+
                 model_prog = flatten([self.index_to_prog[k] for k in rules])
                 _, inconsistent = self.tester.test_prog(model_prog)
                 if inconsistent:
                     con = ':-' + ','.join(f'rule({i})' for i in out) + '.'
                     self.constraints.append(con)
-                    return self.select_solution()
+                    return self.select_solution(settings)
         return flatten([self.index_to_prog[k] for k in out]), incomplete
 
     def update_best_prog(self, prog, pos_covered):
+        size = prog_size(prog)
         self.prog_coverage[prog] = pos_covered
-        self.build_prog_encoding(prog)
+        self.prog_sizes[prog] = size
+        self.update_shit_progs(prog, pos_covered, size)
+
+        # self.build_prog_encoding(prog)
         new_solution, incomplete = self.select_solution()
 
         # if there is no new better solution, do nothing
