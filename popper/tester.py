0498ad7f667dc6378080f9244dab9d54d38529cf
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2022-11-30 19:06:58 -0800

    Add fix for queries containing compound functors.
    Removing all single-quotes from returned string atoms works to get it
    recognized as a term.

--- popper/tester.py
+++ popper/tester.py
@@ -8,7 +8,9 @@ from . util import format_rule, order_rule, order_prog, prog_is_recursive, forma
 class Tester():
 
     def query(self, query, key):
-        return set(next(self.prolog.query(query))[key])
+        result = next(self.prolog.query(query))[key]
+        result = map(lambda s: s.replace("'", "") if type(s) == str else s, result)
+        return set(result)
 
     def bool_query(self, query,):
         return len(list(self.prolog.query(query))) > 0
