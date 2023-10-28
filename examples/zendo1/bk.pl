44fd99ba502f158921f19e6b813c9ba9fe3f6226
Andrew Cropper <andrew.cropper@gmail.com>
2023-10-28 10:05:15 +0100

    added zendo1

---
+++ examples/zendo1/bk.pl
@@ -0,0 +1,931 @@
+
+:-style_check(-discontiguous).
+
+blue(p0_2).
+blue(p10_1).
+blue(p10_3).
+blue(p10_4).
+blue(p11_1).
+blue(p11_4).
+blue(p12_0).
+blue(p13_1).
+blue(p14_1).
+blue(p14_3).
+blue(p15_0).
+blue(p16_0).
+blue(p17_1).
+blue(p18_1).
+blue(p19_1).
+blue(p19_2).
+blue(p1_2).
+blue(p1_3).
+blue(p20_0).
+blue(p20_2).
+blue(p22_2).
+blue(p22_3).
+blue(p23_0).
+blue(p23_1).
+blue(p25_2).
+blue(p26_0).
+blue(p26_3).
+blue(p27_2).
+blue(p28_0).
+blue(p28_1).
+blue(p28_2).
+blue(p28_4).
+blue(p2_0).
+blue(p30_0).
+blue(p30_1).
+blue(p30_2).
+blue(p30_4).
+blue(p31_1).
+blue(p31_2).
+blue(p33_0).
+blue(p33_1).
+blue(p34_2).
+blue(p35_1).
+blue(p36_1).
+blue(p36_2).
+blue(p38_1).
+blue(p39_0).
+blue(p39_2).
+blue(p3_2).
+blue(p3_3).
+blue(p4_2).
+blue(p5_1).
+blue(p5_2).
+blue(p6_1).
+blue(p7_0).
+blue(p7_2).
+blue(p8_2).
+blue(p9_0).
+contact(p0_0, p0_2).
+contact(p0_2, p0_0).
+contact(p10_0, p10_3).
+contact(p10_2, p10_4).
+contact(p10_2, p10_4).
+contact(p10_3, p10_0).
+contact(p10_4, p10_2).
+contact(p10_4, p10_2).
+contact(p11_3, p11_4).
+contact(p11_4, p11_3).
+contact(p12_0, p12_1).
+contact(p12_1, p12_0).
+contact(p13_0, p13_1).
+contact(p13_1, p13_0).
+contact(p14_0, p14_1).
+contact(p14_1, p14_0).
+contact(p15_0, p15_2).
+contact(p15_2, p15_0).
+contact(p16_0, p16_1).
+contact(p16_1, p16_0).
+contact(p17_0, p17_1).
+contact(p17_1, p17_0).
+contact(p18_0, p18_1).
+contact(p18_1, p18_0).
+contact(p19_0, p19_1).
+contact(p19_1, p19_0).
+contact(p1_2, p1_4).
+contact(p1_4, p1_2).
+contact(p22_1, p22_4).
+contact(p22_1, p22_4).
+contact(p22_4, p22_1).
+contact(p22_4, p22_1).
+contact(p25_0, p25_2).
+contact(p25_0, p25_2).
+contact(p25_1, p25_2).
+contact(p25_1, p25_2).
+contact(p25_2, p25_0).
+contact(p25_2, p25_0).
+contact(p25_2, p25_1).
+contact(p25_2, p25_1).
+contact(p28_0, p28_4).
+contact(p28_0, p28_4).
+contact(p28_4, p28_0).
+contact(p28_4, p28_0).
+contact(p2_0, p2_1).
+contact(p2_1, p2_0).
+contact(p30_0, p30_4).
+contact(p30_0, p30_4).
+contact(p30_1, p30_3).
+contact(p30_1, p30_3).
+contact(p30_3, p30_1).
+contact(p30_3, p30_1).
+contact(p30_4, p30_0).
+contact(p30_4, p30_0).
+contact(p3_3, p3_4).
+contact(p3_4, p3_3).
+contact(p4_0, p4_2).
+contact(p4_2, p4_0).
+contact(p5_2, p5_3).
+contact(p5_3, p5_2).
+contact(p6_0, p6_1).
+contact(p6_1, p6_0).
+contact(p7_1, p7_2).
+contact(p7_1, p7_2).
+contact(p7_1, p7_4).
+contact(p7_1, p7_4).
+contact(p7_2, p7_1).
+contact(p7_2, p7_1).
+contact(p7_2, p7_4).
+contact(p7_2, p7_4).
+contact(p7_4, p7_1).
+contact(p7_4, p7_1).
+contact(p7_4, p7_2).
+contact(p7_4, p7_2).
+contact(p8_2, p8_4).
+contact(p8_4, p8_2).
+contact(p9_0, p9_2).
+contact(p9_1, p9_2).
+contact(p9_1, p9_2).
+contact(p9_2, p9_0).
+contact(p9_2, p9_1).
+contact(p9_2, p9_1).
+coord1(p0_0, 1).
+coord1(p0_1, 9).
+coord1(p0_2, 0).
+coord1(p10_0, 8).
+coord1(p10_1, 3).
+coord1(p10_2, 9).
+coord1(p10_3, 8).
+coord1(p10_4, 9).
+coord1(p11_0, 3).
+coord1(p11_1, 10).
+coord1(p11_2, 2).
+coord1(p11_3, 4).
+coord1(p11_4, 4).
+coord1(p12_0, 7).
+coord1(p12_1, 7).
+coord1(p13_0, 8).
+coord1(p13_1, 8).
+coord1(p14_0, 1).
+coord1(p14_1, 1).
+coord1(p14_2, 7).
+coord1(p14_3, 1).
+coord1(p15_0, 2).
+coord1(p15_1, 1).
+coord1(p15_2, 3).
+coord1(p15_3, 10).
+coord1(p16_0, 0).
+coord1(p16_1, -1).
+coord1(p17_0, 3).
+coord1(p17_1, 3).
+coord1(p18_0, 4).
+coord1(p18_1, 4).
+coord1(p18_2, 3).
+coord1(p18_3, 8).
+coord1(p19_0, 3).
+coord1(p19_1, 3).
+coord1(p19_2, 3).
+coord1(p1_0, 5).
+coord1(p1_1, 1).
+coord1(p1_2, 2).
+coord1(p1_3, 4).
+coord1(p1_4, 1).
+coord1(p20_0, 0).
+coord1(p20_1, 5).
+coord1(p20_2, 2).
+coord1(p21_0, 1).
+coord1(p21_1, 9).
+coord1(p21_2, 5).
+coord1(p22_0, 7).
+coord1(p22_1, 6).
+coord1(p22_2, 4).
+coord1(p22_3, 0).
+coord1(p22_4, 6).
+coord1(p23_0, 6).
+coord1(p23_1, 1).
+coord1(p24_0, 8).
+coord1(p24_1, 10).
+coord1(p24_2, 7).
+coord1(p25_0, 2).
+coord1(p25_1, 3).
+coord1(p25_2, 3).
+coord1(p26_0, 5).
+coord1(p26_1, 8).
+coord1(p26_2, 5).
+coord1(p26_3, 3).
+coord1(p27_0, 5).
+coord1(p27_1, 9).
+coord1(p27_2, 7).
+coord1(p28_0, 2).
+coord1(p28_1, 7).
+coord1(p28_2, 1).
+coord1(p28_3, 10).
+coord1(p28_4, 2).
+coord1(p29_0, 4).
+coord1(p29_1, 9).
+coord1(p29_2, 5).
+coord1(p29_3, 2).
+coord1(p2_0, 9).
+coord1(p2_1, 9).
+coord1(p2_2, 5).
+coord1(p2_3, 1).
+coord1(p2_4, 8).
+coord1(p30_0, 5).
+coord1(p30_1, 8).
+coord1(p30_2, 4).
+coord1(p30_3, 8).
+coord1(p30_4, 5).
+coord1(p31_0, 10).
+coord1(p31_1, 7).
+coord1(p31_2, 8).
+coord1(p31_3, 6).
+coord1(p32_0, 1).
+coord1(p33_0, 10).
+coord1(p33_1, 1).
+coord1(p33_2, 9).
+coord1(p34_0, 8).
+coord1(p34_1, 10).
+coord1(p34_2, 9).
+coord1(p34_3, 4).
+coord1(p35_0, 2).
+coord1(p35_1, 10).
+coord1(p36_0, 6).
+coord1(p36_1, 8).
+coord1(p36_2, 7).
+coord1(p36_3, 4).
+coord1(p37_0, 4).
+coord1(p37_1, 2).
+coord1(p38_0, 10).
+coord1(p38_1, 1).
+coord1(p39_0, 5).
+coord1(p39_1, 2).
+coord1(p39_2, 8).
+coord1(p3_0, 10).
+coord1(p3_1, 9).
+coord1(p3_2, 6).
+coord1(p3_3, 5).
+coord1(p3_4, 6).
+coord1(p4_0, 11).
+coord1(p4_1, 2).
+coord1(p4_2, 10).
+coord1(p5_0, 0).
+coord1(p5_1, 5).
+coord1(p5_2, 6).
+coord1(p5_3, 6).
+coord1(p6_0, 6).
+coord1(p6_1, 5).
+coord1(p7_0, 2).
+coord1(p7_1, 7).
+coord1(p7_2, 7).
+coord1(p7_3, 2).
+coord1(p7_4, 8).
+coord1(p8_0, 0).
+coord1(p8_1, 6).
+coord1(p8_2, 3).
+coord1(p8_3, 3).
+coord1(p8_4, 3).
+coord1(p9_0, 0).
+coord1(p9_1, 0).
+coord1(p9_2, 1).
+coord1(p9_3, 0).
+coord2(p0_0, 0).
+coord2(p0_1, 8).
+coord2(p0_2, 0).
+coord2(p10_0, 0).
+coord2(p10_1, 8).
+coord2(p10_2, 2).
+coord2(p10_3, 1).
+coord2(p10_4, 2).
+coord2(p11_0, 3).
+coord2(p11_1, 8).
+coord2(p11_2, 9).
+coord2(p11_3, 8).
+coord2(p11_4, 8).
+coord2(p12_0, 6).
+coord2(p12_1, 5).
+coord2(p13_0, 3).
+coord2(p13_1, 3).
+coord2(p14_0, 2).
+coord2(p14_1, 3).
+coord2(p14_2, 9).
+coord2(p14_3, 5).
+coord2(p15_0, 9).
+coord2(p15_1, 8).
+coord2(p15_2, 9).
+coord2(p15_3, 0).
+coord2(p16_0, 10).
+coord2(p16_1, 10).
+coord2(p17_0, 1).
+coord2(p17_1, 2).
+coord2(p18_0, 8).
+coord2(p18_1, 7).
+coord2(p18_2, 0).
+coord2(p18_3, 0).
+coord2(p19_0, 4).
+coord2(p19_1, 3).
+coord2(p19_2, 0).
+coord2(p1_0, 9).
+coord2(p1_1, 2).
+coord2(p1_2, 9).
+coord2(p1_3, 1).
+coord2(p1_4, 9).
+coord2(p20_0, 2).
+coord2(p20_1, 7).
+coord2(p20_2, 9).
+coord2(p21_0, 1).
+coord2(p21_1, 7).
+coord2(p21_2, 2).
+coord2(p22_0, 1).
+coord2(p22_1, 4).
+coord2(p22_2, 9).
+coord2(p22_3, 6).
+coord2(p22_4, 3).
+coord2(p23_0, 0).
+coord2(p23_1, 1).
+coord2(p24_0, 3).
+coord2(p24_1, 8).
+coord2(p24_2, 1).
+coord2(p25_0, 9).
+coord2(p25_1, 8).
+coord2(p25_2, 9).
+coord2(p26_0, 8).
+coord2(p26_1, 3).
+coord2(p26_2, 2).
+coord2(p26_3, 6).
+coord2(p27_0, 10).
+coord2(p27_1, 6).
+coord2(p27_2, 6).
+coord2(p28_0, 10).
+coord2(p28_1, 9).
+coord2(p28_2, 6).
+coord2(p28_3, 4).
+coord2(p28_4, 10).
+coord2(p29_0, 8).
+coord2(p29_1, 7).
+coord2(p29_2, 5).
+coord2(p29_3, 2).
+coord2(p2_0, 8).
+coord2(p2_1, 9).
+coord2(p2_2, 1).
+coord2(p2_3, 5).
+coord2(p2_4, 7).
+coord2(p30_0, 8).
+coord2(p30_1, 4).
+coord2(p30_2, 2).
+coord2(p30_3, 4).
+coord2(p30_4, 9).
+coord2(p31_0, 3).
+coord2(p31_1, 1).
+coord2(p31_2, 7).
+coord2(p31_3, 10).
+coord2(p32_0, 0).
+coord2(p33_0, 8).
+coord2(p33_1, 2).
+coord2(p33_2, 9).
+coord2(p34_0, 5).
+coord2(p34_1, 3).
+coord2(p34_2, 8).
+coord2(p34_3, 2).
+coord2(p35_0, 8).
+coord2(p35_1, 8).
+coord2(p36_0, 9).
+coord2(p36_1, 5).
+coord2(p36_2, 3).
+coord2(p36_3, 6).
+coord2(p37_0, 7).
+coord2(p37_1, 1).
+coord2(p38_0, 9).
+coord2(p38_1, 5).
+coord2(p39_0, 8).
+coord2(p39_1, 4).
+coord2(p39_2, 5).
+coord2(p3_0, 2).
+coord2(p3_1, 3).
+coord2(p3_2, 5).
+coord2(p3_3, 9).
+coord2(p3_4, 9).
+coord2(p4_0, 10).
+coord2(p4_1, 0).
+coord2(p4_2, 10).
+coord2(p5_0, 4).
+coord2(p5_1, 3).
+coord2(p5_2, 7).
+coord2(p5_3, 6).
+coord2(p6_0, 9).
+coord2(p6_1, 9).
+coord2(p7_0, 2).
+coord2(p7_1, 8).
+coord2(p7_2, 7).
+coord2(p7_3, 8).
+coord2(p7_4, 7).
+coord2(p8_0, 5).
+coord2(p8_1, 1).
+coord2(p8_2, 5).
+coord2(p8_3, 9).
+coord2(p8_4, 5).
+coord2(p9_0, 3).
+coord2(p9_1, 0).
+coord2(p9_2, 3).
+coord2(p9_3, 6).
+green(p10_2).
+green(p11_2).
+green(p15_1).
+green(p15_3).
+green(p18_2).
+green(p18_3).
+green(p20_1).
+green(p21_0).
+green(p22_0).
+green(p22_1).
+green(p22_4).
+green(p24_2).
+green(p25_1).
+green(p26_2).
+green(p27_0).
+green(p27_1).
+green(p28_3).
+green(p29_0).
+green(p29_1).
+green(p2_2).
+green(p30_3).
+green(p31_0).
+green(p31_3).
+green(p33_2).
+green(p34_0).
+green(p34_3).
+green(p36_3).
+green(p37_0).
+green(p3_1).
+green(p4_1).
+green(p5_0).
+green(p7_4).
+green(p8_0).
+green(p9_3).
+large(10).
+large(7).
+large(8).
+large(9).
+lhs(p10_4).
+lhs(p11_0).
+lhs(p11_2).
+lhs(p11_3).
+lhs(p16_1).
+lhs(p17_0).
+lhs(p18_2).
+lhs(p1_0).
+lhs(p1_2).
+lhs(p21_0).
+lhs(p21_2).
+lhs(p23_0).
+lhs(p24_0).
+lhs(p26_0).
+lhs(p26_2).
+lhs(p27_2).
+lhs(p28_0).
+lhs(p28_4).
+lhs(p29_2).
+lhs(p29_3).
+lhs(p2_1).
+lhs(p30_2).
+lhs(p33_0).
+lhs(p33_1).
+lhs(p34_2).
+lhs(p34_3).
+lhs(p3_1).
+lhs(p3_4).
+lhs(p5_3).
+lhs(p8_0).
+lhs(p8_2).
+lhs(p8_3).
+lhs(p9_0).
+lhs(p9_1).
+lhs(p9_2).
+max_size(10).
+medium(4).
+medium(5).
+medium(6).
+piece(0, p0_0).
+piece(0, p0_1).
+piece(0, p0_2).
+piece(1, p1_0).
+piece(1, p1_1).
+piece(1, p1_2).
+piece(1, p1_3).
+piece(1, p1_4).
+piece(10, p10_0).
+piece(10, p10_1).
+piece(10, p10_2).
+piece(10, p10_3).
+piece(10, p10_4).
+piece(11, p11_0).
+piece(11, p11_1).
+piece(11, p11_2).
+piece(11, p11_3).
+piece(11, p11_4).
+piece(12, p12_0).
+piece(12, p12_1).
+piece(13, p13_0).
+piece(13, p13_1).
+piece(14, p14_0).
+piece(14, p14_1).
+piece(14, p14_2).
+piece(14, p14_3).
+piece(15, p15_0).
+piece(15, p15_1).
+piece(15, p15_2).
+piece(15, p15_3).
+piece(16, p16_0).
+piece(16, p16_1).
+piece(17, p17_0).
+piece(17, p17_1).
+piece(18, p18_0).
+piece(18, p18_1).
+piece(18, p18_2).
+piece(18, p18_3).
+piece(19, p19_0).
+piece(19, p19_1).
+piece(19, p19_2).
+piece(2, p2_0).
+piece(2, p2_1).
+piece(2, p2_2).
+piece(2, p2_3).
+piece(2, p2_4).
+piece(20, p20_0).
+piece(20, p20_1).
+piece(20, p20_2).
+piece(21, p21_0).
+piece(21, p21_1).
+piece(21, p21_2).
+piece(22, p22_0).
+piece(22, p22_1).
+piece(22, p22_2).
+piece(22, p22_3).
+piece(22, p22_4).
+piece(23, p23_0).
+piece(23, p23_1).
+piece(24, p24_0).
+piece(24, p24_1).
+piece(24, p24_2).
+piece(25, p25_0).
+piece(25, p25_1).
+piece(25, p25_2).
+piece(26, p26_0).
+piece(26, p26_1).
+piece(26, p26_2).
+piece(26, p26_3).
+piece(27, p27_0).
+piece(27, p27_1).
+piece(27, p27_2).
+piece(28, p28_0).
+piece(28, p28_1).
+piece(28, p28_2).
+piece(28, p28_3).
+piece(28, p28_4).
+piece(29, p29_0).
+piece(29, p29_1).
+piece(29, p29_2).
+piece(29, p29_3).
+piece(3, p3_0).
+piece(3, p3_1).
+piece(3, p3_2).
+piece(3, p3_3).
+piece(3, p3_4).
+piece(30, p30_0).
+piece(30, p30_1).
+piece(30, p30_2).
+piece(30, p30_3).
+piece(30, p30_4).
+piece(31, p31_0).
+piece(31, p31_1).
+piece(31, p31_2).
+piece(31, p31_3).
+piece(32, p32_0).
+piece(33, p33_0).
+piece(33, p33_1).
+piece(33, p33_2).
+piece(34, p34_0).
+piece(34, p34_1).
+piece(34, p34_2).
+piece(34, p34_3).
+piece(35, p35_0).
+piece(35, p35_1).
+piece(36, p36_0).
+piece(36, p36_1).
+piece(36, p36_2).
+piece(36, p36_3).
+piece(37, p37_0).
+piece(37, p37_1).
+piece(38, p38_0).
+piece(38, p38_1).
+piece(39, p39_0).
+piece(39, p39_1).
+piece(39, p39_2).
+piece(4, p4_0).
+piece(4, p4_1).
+piece(4, p4_2).
+piece(5, p5_0).
+piece(5, p5_1).
+piece(5, p5_2).
+piece(5, p5_3).
+piece(6, p6_0).
+piece(6, p6_1).
+piece(7, p7_0).
+piece(7, p7_1).
+piece(7, p7_2).
+piece(7, p7_3).
+piece(7, p7_4).
+piece(8, p8_0).
+piece(8, p8_1).
+piece(8, p8_2).
+piece(8, p8_3).
+piece(8, p8_4).
+piece(9, p9_0).
+piece(9, p9_1).
+piece(9, p9_2).
+piece(9, p9_3).
+red(p0_0).
+red(p0_1).
+red(p10_0).
+red(p11_0).
+red(p11_3).
+red(p12_1).
+red(p13_0).
+red(p14_0).
+red(p14_2).
+red(p15_2).
+red(p16_1).
+red(p17_0).
+red(p18_0).
+red(p19_0).
+red(p1_0).
+red(p1_1).
+red(p1_4).
+red(p21_1).
+red(p21_2).
+red(p24_0).
+red(p24_1).
+red(p25_0).
+red(p26_1).
+red(p29_2).
+red(p29_3).
+red(p2_1).
+red(p2_3).
+red(p2_4).
+red(p32_0).
+red(p34_1).
+red(p35_0).
+red(p36_0).
+red(p37_1).
+red(p38_0).
+red(p39_1).
+red(p3_0).
+red(p3_4).
+red(p4_0).
+red(p5_3).
+red(p6_0).
+red(p7_1).
+red(p7_3).
+red(p8_1).
+red(p8_3).
+red(p8_4).
+red(p9_1).
+red(p9_2).
+rhs(p10_0).
+rhs(p10_1).
+rhs(p10_3).
+rhs(p13_0).
+rhs(p15_0).
+rhs(p15_1).
+rhs(p15_2).
+rhs(p15_3).
+rhs(p18_1).
+rhs(p18_3).
+rhs(p19_0).
+rhs(p19_1).
+rhs(p19_2).
+rhs(p1_1).
+rhs(p1_4).
+rhs(p20_0).
+rhs(p20_2).
+rhs(p22_0).
+rhs(p22_1).
+rhs(p22_3).
+rhs(p24_2).
+rhs(p26_3).
+rhs(p28_1).
+rhs(p29_0).
+rhs(p2_4).
+rhs(p31_0).
+rhs(p31_1).
+rhs(p32_0).
+rhs(p37_1).
+rhs(p38_0).
+rhs(p38_1).
+rhs(p3_3).
+rhs(p4_0).
+rhs(p4_2).
+rhs(p5_0).
+rhs(p5_1).
+rhs(p5_2).
+rhs(p7_4).
+rhs(p8_1).
+size(p0_0, 2).
+size(p0_1, 1).
+size(p0_2, 2).
+size(p10_0, 10).
+size(p10_1, 5).
+size(p10_2, 0).
+size(p10_3, 2).
+size(p10_4, 2).
+size(p11_0, 1).
+size(p11_1, 5).
+size(p11_2, 9).
+size(p11_3, 0).
+size(p11_4, 0).
+size(p12_0, 2).
+size(p12_1, 4).
+size(p13_0, 7).
+size(p13_1, 0).
+size(p14_0, 3).
+size(p14_1, 1).
+size(p14_2, 8).
+size(p14_3, 10).
+size(p15_0, 2).
+size(p15_1, 4).
+size(p15_2, 5).
+size(p15_3, 0).
+size(p16_0, 0).
+size(p16_1, 1).
+size(p17_0, 6).
+size(p17_1, 3).
+size(p18_0, 9).
+size(p18_1, 0).
+size(p18_2, 6).
+size(p18_3, 9).
+size(p19_0, 2).
+size(p19_1, 0).
+size(p19_2, 0).
+size(p1_0, 2).
+size(p1_1, 6).
+size(p1_2, 2).
+size(p1_3, 3).
+size(p1_4, 2).
+size(p20_0, 1).
+size(p20_1, 1).
+size(p20_2, 1).
+size(p21_0, 9).
+size(p21_1, 8).
+size(p21_2, 1).
+size(p22_0, 8).
+size(p22_1, 1).
+size(p22_2, 7).
+size(p22_3, 2).
+size(p22_4, 4).
+size(p23_0, 2).
+size(p23_1, 6).
+size(p24_0, 4).
+size(p24_1, 9).
+size(p24_2, 0).
+size(p25_0, 6).
+size(p25_1, 1).
+size(p25_2, 6).
+size(p26_0, 10).
+size(p26_1, 7).
+size(p26_2, 1).
+size(p26_3, 4).
+size(p27_0, 9).
+size(p27_1, 3).
+size(p27_2, 6).
+size(p28_0, 7).
+size(p28_1, 7).
+size(p28_2, 5).
+size(p28_3, 7).
+size(p28_4, 0).
+size(p29_0, 1).
+size(p29_1, 2).
+size(p29_2, 4).
+size(p29_3, 6).
+size(p2_0, 0).
+size(p2_1, 3).
+size(p2_2, 4).
+size(p2_3, 9).
+size(p2_4, 8).
+size(p30_0, 7).
+size(p30_1, 1).
+size(p30_2, 6).
+size(p30_3, 7).
+size(p30_4, 1).
+size(p31_0, 4).
+size(p31_1, 10).
+size(p31_2, 8).
+size(p31_3, 1).
+size(p32_0, 7).
+size(p33_0, 9).
+size(p33_1, 4).
+size(p33_2, 8).
+size(p34_0, 1).
+size(p34_1, 8).
+size(p34_2, 8).
+size(p34_3, 3).
+size(p35_0, 7).
+size(p35_1, 3).
+size(p36_0, 9).
+size(p36_1, 10).
+size(p36_2, 9).
+size(p36_3, 3).
+size(p37_0, 6).
+size(p37_1, 3).
+size(p38_0, 9).
+size(p38_1, 6).
+size(p39_0, 2).
+size(p39_1, 3).
+size(p39_2, 9).
+size(p3_0, 4).
+size(p3_1, 9).
+size(p3_2, 9).
+size(p3_3, 1).
+size(p3_4, 0).
+size(p4_0, 8).
+size(p4_1, 2).
+size(p4_2, 3).
+size(p5_0, 2).
+size(p5_1, 10).
+size(p5_2, 0).
+size(p5_3, 4).
+size(p6_0, 4).
+size(p6_1, 2).
+size(p7_0, 7).
+size(p7_1, 1).
+size(p7_2, 2).
+size(p7_3, 10).
+size(p7_4, 9).
+size(p8_0, 5).
+size(p8_1, 6).
+size(p8_2, 0).
+size(p8_3, 7).
+size(p8_4, 5).
+size(p9_0, 1).
+size(p9_1, 7).
+size(p9_2, 6).
+size(p9_3, 1).
+small(0).
+small(1).
+small(2).
+small(3).
+strange(p0_1).
+strange(p10_2).
+strange(p11_1).
+strange(p11_4).
+strange(p12_0).
+strange(p13_1).
+strange(p14_3).
+strange(p16_0).
+strange(p23_1).
+strange(p24_1).
+strange(p25_0).
+strange(p25_1).
+strange(p2_0).
+strange(p2_3).
+strange(p30_0).
+strange(p30_1).
+strange(p30_3).
+strange(p31_2).
+strange(p33_2).
+strange(p35_0).
+strange(p36_0).
+strange(p36_1).
+strange(p37_0).
+strange(p39_0).
+strange(p39_1).
+strange(p3_2).
+strange(p6_0).
+strange(p7_2).
+strange(p7_3).
+strange(p8_4).
+strange(p9_3).
+upright(p0_0).
+upright(p0_2).
+upright(p12_1).
+upright(p14_0).
+upright(p14_1).
+upright(p14_2).
+upright(p17_1).
+upright(p18_0).
+upright(p1_3).
+upright(p20_1).
+upright(p21_1).
+upright(p22_2).
+upright(p22_4).
+upright(p25_2).
+upright(p26_1).
+upright(p27_0).
+upright(p27_1).
+upright(p28_2).
+upright(p28_3).
+upright(p29_1).
+upright(p2_2).
+upright(p30_4).
+upright(p31_3).
+upright(p34_0).
+upright(p34_1).
+upright(p35_1).
+upright(p36_2).
+upright(p36_3).
+upright(p39_2).
+upright(p3_0).
+upright(p4_1).
+upright(p6_1).
+upright(p7_0).
+upright(p7_1).
