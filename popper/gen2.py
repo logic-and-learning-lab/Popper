e45550c80bc0493358449c44894234ff4670fded
Andrew Cropper <andrew.cropper@gmail.com>
2025-05-19 11:24:23 +0300

    minor fix for issue with body pred declarations

--- popper/gen2.py
+++ popper/gen2.py
@@ -48,8 +48,9 @@ class Generator:
         bias_text = re.sub(r'max_body\(\d*\).','', bias_text)
         bias_text = re.sub(r'max_clauses\(\d*\).','', bias_text)
 
+        # AC: NEED TO COMPLETELY REFACTOR THIS CODE
         for p,a in settings.pointless:
-            bias_text = re.sub(rf'body_pred\({p},{a}\).','', bias_text)
+            bias_text = re.sub(rf'body_pred\({p},\s*{a}\)\.', '', bias_text)
             bias_text = re.sub(rf'constant\({p},.*?\).*', '', bias_text, flags=re.MULTILINE)
 
         encoding.append(bias_text)
